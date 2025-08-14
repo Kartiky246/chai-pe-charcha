14652bd1f4fe9654229cd72c2c27976b204fd32e
kartiky246 <kartiky246@gmail.com>
2025-08-15 00:34:37 +0530

    fix(): remove duplicate handler

--- src/ai-chat/ai-chat.controller.ts
+++ src/ai-chat/ai-chat.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Query, Res } from '@nestjs/common';
 import { ChatMessageDto } from './dto/chat-message-dto';
 import type { Response } from 'express';
 import { AiChatService } from './ai-chat.service';
@@ -7,26 +7,15 @@ import { AiChatService } from './ai-chat.service';
 export class AiChatController {
   constructor(private readonly aiChatService: AiChatService) {}
 
-  @Post('/hitesh')
-  async sendMessageToHitesh(@Res() res: Response, @Body() payload: ChatMessageDto) {
-    res.setHeader("Content-Type", "text/event-stream");
-    res.setHeader("Cache-Control", "no-cache");
-    const {message} = payload;
-    await this.aiChatService.getAiChatStream(message,(chunk)=>{
-      res.write(`data: ${chunk}\n\n`);
-    }, 'hitesh');
-    res.end();
-  }
-
-  @Post('/piyush')
-  async sendMessageToPiyush(@Res() res: Response, @Body() payload: ChatMessageDto) {
+  @Post()
+  async sendMessageToPiyush(@Res() res: Response, @Body() payload: ChatMessageDto, @Query('id') id: string) {
     res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Cache-Control", "no-cache");
     const {message} = payload;
     await this.aiChatService.getAiChatStream(message,(chunk)=>{
       console.log("Res-", chunk)
       res.write(`data: ${chunk}\n\n`);
-    }, 'piyush');
+    }, id);
     res.end();
   }
 }
